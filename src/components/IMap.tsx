bee539771b1cb96d096a984460740de430a54165
Sam Ling <sam@thling.com>
2020-08-25 02:46:03 -0700

    Fix rendering dependency

--- src/components/IMap.tsx
+++ src/components/IMap.tsx
@@ -17,44 +17,46 @@ function setUpGoogleMapsAPI(div: HTMLDivElement, onScriptLoaded: () => void) {
     'src',
     `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPS_API_KEY}&v=beta&map_ids=815851252755dae4`
   );
+  script.onload = onScriptLoaded;
 
   div.appendChild(script);
-
-  script.onload = onScriptLoaded;
 }
 
 function IMap() {
-  const [mapCentre, setMapCentre] = useState<number[]>([47.6062, -122.3321]);
+  const [mapCentre, setMapCentre] = useState<google.maps.LatLngLiteral>({
+    lat: 41.8781,
+    lng: -87.6298,
+  });
+
+  const [mapElement, setMapElement] = useState<google.maps.Map<HTMLDivElement> | null>(null);
 
   // Get user geolocation on monut
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-
-        setMapCentre([latitude, longitude]);
-      });
+      navigator.geolocation.getCurrentPosition(({ coords }) =>
+        setMapCentre({ lat: coords.latitude, lng: coords.longitude })
+      );
     }
   }, []);
 
+  useEffect(() => mapElement?.setCenter(mapCentre), [mapCentre, mapElement]);
+
   // Setup Maps
-  const initMapCallback = useCallback(
-    (div: HTMLDivElement | null) => {
-      if (div !== null) {
-        setUpGoogleMapsAPI(div, () => {
-          const mapElement = new google.maps.Map(div, {
+  const initMapCallback = useCallback((div: HTMLDivElement | null) => {
+    if (div !== null) {
+      setUpGoogleMapsAPI(div, () =>
+        setMapElement(
+          new google.maps.Map(div, {
             zoom: 12,
             mapTypeControl: false,
             streetViewControl: false,
+            fullscreenControl: false,
             mapId: '815851252755dae4',
-          });
-
-          mapElement.setCenter(new google.maps.LatLng(mapCentre[0], mapCentre[1]));
-        });
-      }
-    },
-    [mapCentre]
-  );
+          })
+        )
+      );
+    }
+  }, []);
 
   return <MapContainer ref={initMapCallback} />;
 }
