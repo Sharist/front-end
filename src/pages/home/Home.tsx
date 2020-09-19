77a565579b12dfdbe024ee07d8455e482eb8fba1
Sam Ling <sam@thling.com>
2020-09-18 22:21:15 -0700

    Redirect to /plan / if user is signed in
    resolves #43

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouteComponentProps } from '@reach/router';
 
-import LayoutContainer from '../../components/LayoutContainer';
+import { useAuthentication } from '../../common/hooks/useAuthentication';
 import Landing from './Landing';
+import LayoutContainer from '../../components/LayoutContainer';
+import routes from '../../routes';
 
 function Home(_: RouteComponentProps) {
+  const { signedIn } = useAuthentication({ requestLogin: false });
+
+  useEffect(() => {
+    signedIn && routes.plan.navigator();
+  }, [signedIn]);
+
   return (
     <LayoutContainer center fullHeight floatingHeader noPadding noMargin isLanding>
       <Landing />
