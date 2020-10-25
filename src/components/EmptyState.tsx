e11c8a5f33c9b1d6f5d04f9f0c684f30fe5fc0db
Sam Ling <sam@thling.com>
2020-10-25 00:15:27 -0700

    Implement "empty state" component for filling up empty pages.

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+import { GiAirplane } from 'react-icons/gi';
+import { Palette } from '../common/themes';
+import Button from './Button';
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
+
+const EmptyStateImage = styled.div`
+  ${({ theme: { breakpoints } }) => css`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin: 10rem 0 -2.5rem 0;
+    transform: rotate(-15deg);
+
+    @media screen and (max-width: ${breakpoints.MOBILE}) {
+      margin-top: 0;
+    }
+  `}
+`;
+
+const Title = styled.p`
+  ${({ theme: { typography, breakpoints } }) => css`
+    font-size: ${typography.X_LARGE};
+    margin-bottom: 0.5rem;
+    text-align: center;
+
+    @media screen and (max-width: ${breakpoints.MOBILE}) {
+      font-size: ${typography.LARGE};
+    }
+  `}
+`;
+
+const Subtitle = styled.p`
+  margin: 0.5rem 0 2rem 0;
+  text-align: center;
+`;
+
+type Props = {
+  title: string;
+  subtitle: string;
+  action?: {
+    actionLabel: string;
+    actionHandler: () => void;
+  };
+};
+
+function EmptyState({ title, subtitle, action }: Props) {
+  return (
+    <Wrapper>
+      <EmptyStateImage>
+        <GiAirplane size='90%' color={Palette.ASH_DARKER} />
+      </EmptyStateImage>
+
+      <Title>{title}</Title>
+      <Subtitle>{subtitle}</Subtitle>
+      {action && (
+        <Button onClick={action.actionHandler} isPrimary>
+          {action.actionLabel}
+        </Button>
+      )}
+    </Wrapper>
+  );
+}
+
+export default EmptyState;
